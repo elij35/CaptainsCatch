4a940078923a37e80cc27bbaa777cf08ccb9e75f
Eli Bowen <elijbowen@gmail.com>
2023-11-02 19:50:13 +0000

    Set up the basic file structure required for the app

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Comp2000 Restaurant app"
+include(":app")
