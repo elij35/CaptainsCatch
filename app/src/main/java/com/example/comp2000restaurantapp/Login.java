23b54887813ce4a5f7d79654b12b03e2465d89da
Eli Bowen <elijbowen@gmail.com>
2023-12-03 19:24:18 +0000

    Login page now takes inputs

--- app/src/main/java/com/example/comp2000restaurantapp/Login.java
+++ app/src/main/java/com/example/comp2000restaurantapp/Login.java
@@ -1,8 +1,14 @@
 package com.example.comp2000restaurantapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +16,43 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        login();
+        loadRegister();
     }
+
+    private void login() {
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.login_btn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(Login.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login.this, Home.class);
+                    startActivity(intent);
+                } else
+                    //incorrect
+                    Toast.makeText(Login.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadRegister() {
+        TextView manage_bookings = findViewById(R.id.no_account);
+        manage_bookings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
