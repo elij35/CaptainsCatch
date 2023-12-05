61a2d32dccf11760ee1af8236bf7c09dd3a4f606
Eli Bowen <elijbowen@gmail.com>
2023-12-05 15:05:04 +0000

    Basic drop down menu working on bookings page

--- app/src/main/java/com/example/comp2000restaurantapp/NewBooking.java
+++ app/src/main/java/com/example/comp2000restaurantapp/NewBooking.java
@@ -3,8 +3,10 @@ package com.example.comp2000restaurantapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,6 +21,21 @@ public class NewBooking extends AppCompatActivity {
         loadManageBookings();
         loadAvailableTables();
         loadBottomBar();
+        mealtime();
+    }
+
+    private void mealtime() {
+        Spinner staticSpinner = findViewById(R.id.static_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this,
+                R.array.mealtime, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        staticSpinner.setAdapter(staticAdapter);
     }
 
     private void loadBottomBar() {
