e8fdc05684c6e8cf650b70a0a2064be2313c87e5
Eli Bowen <elijbowen@gmail.com>
2023-12-05 17:57:15 +0000

    Improved login page

--- app/src/main/java/com/example/comp2000restaurantapp/Login.java
+++ app/src/main/java/com/example/comp2000restaurantapp/Login.java
@@ -1,10 +1,10 @@
 package com.example.comp2000restaurantapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,41 +18,28 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
 
         login();
-        loadRegister();
     }
 
     private void login() {
 
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
-
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.login_btn);
 
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                    //correct
-                    Toast.makeText(Login.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(Login.this, Home.class);
-                    startActivity(intent);
-                } else
-                    //incorrect
-                    Toast.makeText(Login.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
 
-    private void loadRegister() {
-        TextView manage_bookings = findViewById(R.id.no_account);
-        manage_bookings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this, Register.class);
-                startActivity(intent);
-            }
-        });
+        String data = preferences.getString("login", null);
 
+        loginbtn.setOnClickListener(v -> {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("user", data);
+            editor.putString("password", password.getText().toString());
+            editor.apply();
+
+            Intent intent = new Intent(Login.this, Home.class);
+            startActivity(intent);
+
+        });
     }
 
 }
