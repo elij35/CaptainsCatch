6dc6347ec183c8e9db816e11131b05d0fd12d539
Eli Bowen <elijbowen@gmail.com>
2023-12-07 15:40:39 +0000

    API is now working

--- app/src/main/java/com/example/comp2000restaurantapp/NewBooking.java
+++ app/src/main/java/com/example/comp2000restaurantapp/NewBooking.java
@@ -2,6 +2,7 @@ package com.example.comp2000restaurantapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -10,7 +11,28 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
 public class NewBooking extends AppCompatActivity {
+
+    public String mealtime;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "https://web.socem.plymouth.ac.uk/COMP2000/ReservationApi/api/Reservations";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +64,7 @@ public class NewBooking extends AppCompatActivity {
     }
 
     private void mealtime() {
+        String[] spinnerItems = getResources().getStringArray(R.array.mealtime);
         Spinner staticSpinner = findViewById(R.id.meal_time_spinner);
 
         // Create an ArrayAdapter using the string array and a default spinner
@@ -114,11 +137,71 @@ public class NewBooking extends AppCompatActivity {
         bookings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //getData();
+
                 Intent intent = new Intent(NewBooking.this, Success.class);
                 startActivity(intent);
             }
         });
+
     }
+
+    private void getData() {
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(NewBooking.this);
+            String URL = "https://web.socem.plymouth.ac.uk/COMP2000/ReservationApi/api/Reservations/";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("customerName", "Batman");
+            jsonBody.put("customerPhoneNumber", "07777775776");
+            jsonBody.put("meal", "Lunch");
+            jsonBody.put("seatingArea", "Outside");
+            jsonBody.put("tableSize", 2);
+            jsonBody.put("date", "2024-04-22");
+            final String requestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private void loadManageBookings() {
         Button bookings = findViewById(R.id.manage_bookings);
