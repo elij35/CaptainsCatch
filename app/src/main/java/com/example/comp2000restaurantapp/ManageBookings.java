08c92795c7abc0b0bdab7e230d697abb1bf96b5d
Eli Bowen <elijbowen@gmail.com>
2023-12-08 23:52:02 +0000

    Fixed formatting

--- app/src/main/java/com/example/comp2000restaurantapp/ManageBookings.java
+++ app/src/main/java/com/example/comp2000restaurantapp/ManageBookings.java
@@ -2,7 +2,6 @@ package com.example.comp2000restaurantapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -47,12 +46,9 @@ public class ManageBookings extends AppCompatActivity {
 
     private void loadBackToBookings() {
         Button bookings = findViewById(R.id.back_to_bookings);
-        bookings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ManageBookings.this, NewBooking.class);
-                startActivity(intent);
-            }
+        bookings.setOnClickListener(view -> {
+            Intent intent = new Intent(ManageBookings.this, NewBooking.class);
+            startActivity(intent);
         });
     }
 }
