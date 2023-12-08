710cb8222ebb08ed457cb51edf19ca5adad224cf
Eli Bowen <elijbowen@gmail.com>
2023-12-08 23:59:08 +0000

    Added a void that sends data from json to the API

--- app/src/main/java/com/example/comp2000restaurantapp/storage.java
+++ app/src/main/java/com/example/comp2000restaurantapp/storage.java
@@ -1,19 +1,21 @@
 package com.example.comp2000restaurantapp;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
 
-public class storage {
+public class Storage {
+
     public static void writeJson(Context context, String fileName, String jsonString) {
         try {
             FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
@@ -30,17 +32,41 @@ public class storage {
             InputStream inputStream = Files.newInputStream(Paths.get(fileName));
             JsonNode jsonNode = objectMapper.readTree(inputStream);
 
-            /*
-            //For debugging
-            for (JsonNode value : jsonNode) {
-                Log.i("My tag here", "Value of 'key': " + value);
-            }
-
-             */
             inputStream.close();
-
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void sendToApi(Context context, String accountsFilePath, String loginFilePath) throws RuntimeException {
+        try {
+            ObjectMapper accountsMap = new ObjectMapper();
+            ObjectMapper loginMap = new ObjectMapper();
+
+            InputStream accountsRead = Files.newInputStream(Paths.get(accountsFilePath));
+            InputStream loginRead = Files.newInputStream(Paths.get(loginFilePath));
+
+            JsonNode accountsContent = accountsMap.readTree(accountsRead);
+            JsonNode loginContent = loginMap.readTree(loginRead);
+
+            JSONObject jsonBody = new JSONObject();
+
+            jsonBody.put("customerName", loginContent.get("customerName"));
+            jsonBody.put("customerPhoneNumber", loginContent.get("customerPhoneNumber"));
+            jsonBody.put( "meal", accountsContent.get("meal"));
+            jsonBody.put( "seatingArea", accountsContent.get("seatingArea"));
+            jsonBody.put("tableSize", accountsContent.get("tableSize"));
+            jsonBody.put( "date", accountsContent.get("date"));
+            final String requestBody = jsonBody.toString();
+
+            API.apiSendData(context, requestBody);
+
+            accountsRead.close();
+            loginRead.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
         }
     }
 }
