da71098f17ae8ba4afd4585c9a04a9f20ca5e8cb
Eli Bowen <elijbowen@gmail.com>
2023-12-09 23:53:55 +0000

    Moved strings and improved the API code

--- app/src/main/java/com/example/comp2000restaurantapp/API.java
+++ app/src/main/java/com/example/comp2000restaurantapp/API.java
@@ -1,5 +1,7 @@
 package com.example.comp2000restaurantapp;
 
+import static com.example.comp2000restaurantapp.Storage.readJson;
+
 import android.content.Context;
 import android.util.Log;
 
@@ -10,11 +12,25 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class API {
 
-    public static void apiSendData(Context context, String jsonString) {
+    public static void apiSendData(Context context, String bookingFilePath, String loginFilePath) throws IOException, JSONException {
+
+        //Reading the data in the json files and inputting them into a JSON object to be sent by the API
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("customerName", readJson(loginFilePath).get("customerName").textValue());
+        jsonBody.put("customerPhoneNumber", readJson(loginFilePath).get("customerPhoneNumber").textValue());
+        jsonBody.put("meal", readJson(bookingFilePath).get("meal").textValue());
+        jsonBody.put("seatingArea", readJson(bookingFilePath).get("seatingArea").textValue());
+        jsonBody.put("tableSize", readJson(bookingFilePath).get("tableSize").textValue());
+        jsonBody.put("date", readJson(bookingFilePath).get("date").textValue());
+        final String requestBody = jsonBody.toString();
 
         RequestQueue requestQueue = Volley.newRequestQueue(context);
         String URL = "https://web.socem.plymouth.ac.uk/COMP2000/ReservationApi/api/Reservations/";
@@ -37,10 +53,9 @@ public class API {
 
             @Override
             public byte[] getBody() {
-                return jsonString.getBytes(StandardCharsets.UTF_8);
+                return requestBody.getBytes(StandardCharsets.UTF_8);
             }
         };
-
         requestQueue.add(stringRequest);
     }
 }
