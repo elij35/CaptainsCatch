3220539bc04fe069e3b4da99f398c8d964e8bb37
Eli Bowen <elijbowen@gmail.com>
2023-12-10 00:00:10 +0000

    Deleted API void and optimised read & write

--- app/src/main/java/com/example/comp2000restaurantapp/storage.java
+++ app/src/main/java/com/example/comp2000restaurantapp/storage.java
@@ -16,57 +16,17 @@ import java.nio.file.Paths;
 
 public class Storage {
 
-    public static void writeJson(Context context, String fileName, String jsonString) {
-        try {
-            FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
-            fileOutputStream.write(jsonString.getBytes());
-            fileOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void writeJson(Context context, String fileName, String jsonString) throws IOException {
+        FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+        fileOutputStream.write(jsonString.getBytes());
+        fileOutputStream.close();
     }
 
-    public static void readJson(String fileName) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            InputStream inputStream = Files.newInputStream(Paths.get(fileName));
-            JsonNode jsonNode = objectMapper.readTree(inputStream);
-
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void sendToApi(Context context, String accountsFilePath, String loginFilePath) throws RuntimeException {
-        try {
-            ObjectMapper accountsMap = new ObjectMapper();
-            ObjectMapper loginMap = new ObjectMapper();
-
-            InputStream accountsRead = Files.newInputStream(Paths.get(accountsFilePath));
-            InputStream loginRead = Files.newInputStream(Paths.get(loginFilePath));
-
-            JsonNode accountsContent = accountsMap.readTree(accountsRead);
-            JsonNode loginContent = loginMap.readTree(loginRead);
-
-            JSONObject jsonBody = new JSONObject();
-
-            jsonBody.put("customerName", loginContent.get("customerName"));
-            jsonBody.put("customerPhoneNumber", loginContent.get("customerPhoneNumber"));
-            jsonBody.put( "meal", accountsContent.get("meal"));
-            jsonBody.put( "seatingArea", accountsContent.get("seatingArea"));
-            jsonBody.put("tableSize", accountsContent.get("tableSize"));
-            jsonBody.put( "date", accountsContent.get("date"));
-            final String requestBody = jsonBody.toString();
-
-            API.apiSendData(context, requestBody);
-
-            accountsRead.close();
-            loginRead.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
+    public static JsonNode readJson(String fileName) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        InputStream inputStream = Files.newInputStream(Paths.get(fileName));
+        JsonNode jsonNode = objectMapper.readTree(inputStream);
+        inputStream.close();
+        return jsonNode;
     }
 }
