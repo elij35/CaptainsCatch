8cfa64bed71ab35ea2c46e8927c11de5ee5c0f75
Eli Bowen <elijbowen@gmail.com>
2023-12-10 23:47:52 +0000

    Removed imports

--- app/src/main/java/com/example/comp2000restaurantapp/storage.java
+++ app/src/main/java/com/example/comp2000restaurantapp/storage.java
@@ -5,9 +5,6 @@ import android.content.Context;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
