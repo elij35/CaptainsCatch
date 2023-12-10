2bd6cf64fc364379c5411501239b1a5e842f6044
Eli Bowen <elijbowen@gmail.com>
2023-12-10 17:35:06 +0000

    Added functionality for bottom bar

--- app/src/main/java/com/example/comp2000restaurantapp/Error.java
+++ app/src/main/java/com/example/comp2000restaurantapp/Error.java
@@ -1,6 +1,8 @@
 package com.example.comp2000restaurantapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,32 @@ public class Error extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.error);
+        loadBottomBar();
+    }
+
+    private void loadBottomBar() {
+        ImageButton home = findViewById(R.id.home_icon);
+        home.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+        });
+
+        ImageButton bookings = findViewById(R.id.calendar_icon);
+        bookings.setOnClickListener(view -> {
+            Intent intent = new Intent(this, NewBooking.class);
+            startActivity(intent);
+        });
+
+        ImageButton reviews = findViewById(R.id.reviews_icon);
+        reviews.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Reviews.class);
+            startActivity(intent);
+        });
+
+        ImageButton settings = findViewById(R.id.account_icon);
+        settings.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
+        });
     }
 }
