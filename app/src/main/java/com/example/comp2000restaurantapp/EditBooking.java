a16e7d7d2a4cc2a76a41b3485d77600f623abd2b
Eli Bowen <elijbowen@gmail.com>
2023-12-10 13:23:09 +0000

    Created spinners for editing the booking

--- app/src/main/java/com/example/comp2000restaurantapp/EditBooking.java
+++ app/src/main/java/com/example/comp2000restaurantapp/EditBooking.java
@@ -1,23 +1,196 @@
 package com.example.comp2000restaurantapp;
 
+import static com.example.comp2000restaurantapp.Date.makeDateString;
+import static com.example.comp2000restaurantapp.Date.makeJsonDateString;
+
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Calendar;
+
 public class EditBooking extends AppCompatActivity {
+    String dateSelected;
+    String mealtimeSelected;
+    String locationSelected;
+    String tableSizeSelected;
+
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.edit_booking);
+        setContentView(R.layout.new_booking);
 
-        loadManageBookings();
         loadAmendSuccess();
+        loadManageBookings();
+        loadAvailableTables();
         loadBottomBar();
+        mealtime();
+        location();
+        table_size();
+        initDatePicker();
+
+        dateButton = findViewById(R.id.datePickerButton);
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = (datePicker, year, month, day) -> {
+            month = month + 1;
+            String date = makeDateString(day, month, year);
+            dateButton.setText(date);
+            dateSelected = makeJsonDateString(year, month, day);
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(this, dateSetListener, year, month, day);
+    }
+
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
+    }
+
+    private void table_size() {
+        Spinner staticSpinner = findViewById(R.id.table_size_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this,
+                R.array.table_size, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        staticSpinner.setPrompt("Select your favorite Planet!");
+
+
+        // Apply the adapter to the spinner
+        staticSpinner.setAdapter(staticAdapter);
+
+        staticSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Get the selected item
+                tableSizeSelected = (String) parentView.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                Toast.makeText(getApplicationContext(), "A date must be selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void mealtime() {
+        Spinner staticSpinner = findViewById(R.id.meal_time_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this,
+                R.array.mealtime, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        staticSpinner.setAdapter(staticAdapter);
+
+        staticSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Get the selected item
+                mealtimeSelected = (String) parentView.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                Toast.makeText(getApplicationContext(), "A date must be selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void location() {
+        Spinner staticSpinner = findViewById(R.id.location_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this,
+                R.array.location, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        staticSpinner.setAdapter(staticAdapter);
+
+        staticSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Get the selected item
+                locationSelected = (String) parentView.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                Toast.makeText(getApplicationContext(), "A date must be selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadAmendSuccess() {
+        Button bookings = findViewById(R.id.amend_booking_btn);
+
+        bookings.setOnClickListener(view -> {
+
+            try {
+                writeJson();
+                sendAPI();
+                sendNotification();
+
+            } catch (JSONException | IOException e) {
+                throw new RuntimeException(e);
+            }
+            Intent intent = new Intent(this, BookingAmendSuccess.class);
+            startActivity(intent);
+        });
+    }
+
+    private void sendAPI() throws JSONException, IOException {
+        String accounts = getFilesDir() + "/" + "booking.json";
+        String login = getFilesDir() + "/" + "login.json";
+        API.apiSendData(getApplicationContext(), accounts, login);
+    }
+
+    private void sendNotification() throws IOException {
+        String title = "Successful booking";
+        String body = "Your booking has been confirmed";
+        Notifications.notification(getApplicationContext(), title, body);
+    }
+
+    private void writeJson() throws JSONException, IOException {
+        String fileName = "booking.json";
+        JSONObject jsonBody = new JSONObject();
+
+        jsonBody.put("meal", mealtimeSelected);
+        jsonBody.put("seatingArea", locationSelected);
+        jsonBody.put("tableSize", tableSizeSelected);
+        jsonBody.put("date", dateSelected);
+        final String requestBody = jsonBody.toString();
+        Storage.writeJson(getApplicationContext(), fileName, requestBody);
     }
 
     private void loadBottomBar() {
@@ -43,28 +216,6 @@ public class EditBooking extends AppCompatActivity {
         settings.setOnClickListener(view -> {
             Intent intent = new Intent(this, Settings.class);
             startActivity(intent);
-        });
-    }
-
-    private void loadManageBookings() {
-        Button bookings = findViewById(R.id.manage_bookings);
-        bookings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(EditBooking.this, ManageBookings.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void loadAmendSuccess() {
-        Button bookings = findViewById(R.id.amend_booking_btn);
-        bookings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(EditBooking.this, Success.class);
-                startActivity(intent);
-            }
         });
     }
 }
