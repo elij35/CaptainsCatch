bc5480375b3f73d1934d717aff8958742c028e18
Eli Bowen <elijbowen@gmail.com>
2023-12-10 15:59:55 +0000

    Reviews page done

--- app/src/main/java/com/example/comp2000restaurantapp/Reviews.java
+++ app/src/main/java/com/example/comp2000restaurantapp/Reviews.java
@@ -1,10 +1,20 @@
 package com.example.comp2000restaurantapp;
 
+import static com.example.comp2000restaurantapp.Storage.readJson;
+
 import android.content.Intent;
+import android.graphics.fonts.FontStyle;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.text.ParseException;
 
 public class Reviews extends AppCompatActivity {
 
@@ -13,6 +23,30 @@ public class Reviews extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reviews);
         loadBottomBar();
+        showSubmittedReview();
+    }
+
+    private void showSubmittedReview() {
+        String login = getFilesDir() + "/" + "login.json";
+        Button postReview = findViewById(R.id.submit_review_btn);
+
+        postReview.setOnClickListener(view -> {
+            TextView review = findViewById(R.id.review_input_box);
+            RatingBar stars = findViewById(R.id.ratingBar);
+            TextView person = findViewById(R.id.showReviewer);
+            TextView message = findViewById(R.id.showReviewMessage);
+
+            Float rating = stars.getRating();
+
+            try {
+                String reviewerName = readJson(login).get("customerName").textValue();
+                person.setText(MessageFormat.format("{0} {1} {2} {3}", reviewerName, ",", rating, "stars:"));
+                message.setText(MessageFormat.format("{0} {1}", "Says:", review.getText()));
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     private void loadBottomBar() {
