7702bfcf6a81dc57dd52faaa0ff562da47d6f38a
Eli Bowen <elijbowen@gmail.com>
2023-12-11 00:48:40 +0000

    Creating design for available tables

--- app/src/main/java/com/example/comp2000restaurantapp/AvailableTables.java
+++ app/src/main/java/com/example/comp2000restaurantapp/AvailableTables.java
@@ -1,17 +1,26 @@
 package com.example.comp2000restaurantapp;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Date;
+import java.util.Calendar;
 
 public class AvailableTables extends AppCompatActivity {
-    private Date date;
+
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
+    String dateSelected;
+    String tableSizeSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,51 @@ public class AvailableTables extends AppCompatActivity {
 
         loadBackToBookings();
         loadBottomBar();
+        initDatePicker();
+        table_size();
+    }
+
+    private void initDatePicker() {
+        dateButton = findViewById(R.id.datePickerButton);
+        DatePickerDialog.OnDateSetListener dateSetListener = (datePicker, year, month, day) -> {
+            month = month + 1;
+            String date = DateNow.makeDateString(day, month, year);
+            dateButton.setText(date);
+            dateSelected = DateNow.makeJsonDateString(year, month, day);
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(this, dateSetListener, year, month, day);
+    }
+
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
+    }
+
+    private void table_size() {
+        Spinner staticSpinner = findViewById(R.id.table_size_spinner);
+
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this,
+                R.array.table_size, android.R.layout.simple_spinner_item);
+
+        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        staticSpinner.setAdapter(staticAdapter);
+
+        staticSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                tableSizeSelected = (String) parentView.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                Toast.makeText(getApplicationContext(), "A date must be selected", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void loadBottomBar() {
